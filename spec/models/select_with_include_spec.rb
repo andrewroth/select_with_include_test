9186fd877a13512efeaf0244509d8473e62ea768
Andrew Roth <andrewroth@gmail.com>
2011-12-02 01:39:38 -0500

    use bundler to install gems

--- spec/models/select_with_include_spec.rb
+++ spec/models/select_with_include_spec.rb
@@ -1,10 +1,10 @@
 gem 'factory_girl', '2.0.1'
 require 'factory_girl'
-require 'spec/factories/posts.rb'
+require File.expand_path(File.dirname(__FILE__) + "/../../spec/factories/posts")
 require File.expand_path(File.dirname(__FILE__) + "/../../config/environment")
 
 describe "select_with_include" do
-  it "should do stuff" do
+  it "should select only the columns specified" do
     post = FactoryGirl.build(:post)
     post.comments << FactoryGirl.build(:comment)
     post.comments << FactoryGirl.build(:comment)
@@ -15,5 +15,6 @@ describe "select_with_include" do
 
     ps = Post.find(:all, :include => [ :comments ], :select => "posts.title, comments.id")
     ps.first.comments.respond_to?(:title).should be_false
+    ps.first.comments.respond_to?(:id).should be_true
   end
 end
